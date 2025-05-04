5f4f6dd43f615bf3632723ad6b32fe935796aceb
Brando <thaogbrando@yahoo.com>
2025-05-04 09:16:45 -0600

    Update hobby.tsx

--- app/_components/hobbies/hobby.tsx
+++ app/_components/hobbies/hobby.tsx
@@ -7,7 +7,7 @@ const Hobby = ({ emoji, text }: {emoji: any, text: string}) => {
           <div className="bg-white h-[60px] w-[60px] flex items-center rounded-lg justify-center hover:scale-105 transform transition-all duration-500 text-3xl border-4 hover:border-purple-600">
             {emoji}
           </div>
-          <p className="text-black">{text}</p>
+          <p className="text-black text-sm">{text}</p>
         </div>
         <div className="w-[100px] h-[200px] absolute top-[50px] -z-10"></div>
       </div>
