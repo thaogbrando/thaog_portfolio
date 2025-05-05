62a698c5acaeaa480fafd75502a07059a62dfe9a
Brando <thaogbrando@yahoo.com>
2025-05-05 10:23:20 -0600

    Update NavbarLinks.tsx

--- app/_components/Navbar/NavbarLinks.tsx
+++ app/_components/Navbar/NavbarLinks.tsx
@@ -45,6 +45,7 @@ const NavbarLinks = ({ href, handleClick, offSet }: {
   return (
     <Link
       scroll
+      prefetch
       onClick={handleClick}
       className={`px-4 py-2 mt-2 text-base tracking-wide font-semibold rounded-lg md:mt-0 md:ml-2 transition-colors duration-200 ${
         isActive() 
