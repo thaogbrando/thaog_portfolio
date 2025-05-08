fd56f82c971f1682444c793cb0e484873ee8353a
Brando <thaogbrando@yahoo.com>
2025-05-08 03:15:23 -0600

    Update SkillCard.tsx

--- app/_components/Integrations/SkillCard.tsx
+++ app/_components/Integrations/SkillCard.tsx
@@ -6,7 +6,6 @@ const SkillCard = ({ icon, title, tech }: { icon: string, title: string, tech: s
         <div
             className='flex flex-col gap-4 items-center justify-center p-6 h-80 cursor-pointer bg-blue-secondary/20 dark:bg-dark-text/5 rounded-lg hover:border-[0.1px] shadow-lg hover:border-blue-primary hover:dark:border-light-accent'
         >
-            <Tooltip content={title} />
             <div className='flex items-center flex-col gap-2 my-4 justify-center text-blue-primary dark:text-light-accent font-semibold'>
                 <Icon icon={icon} className='w-14 h-14' />
                 <p className='text-xl underline underline-offset-2'>{title}</p>
