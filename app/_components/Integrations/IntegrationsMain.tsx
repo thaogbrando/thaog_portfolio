c0051489e7769681572c97bdd9be4c30e58a68c7
Brando <thaogbrando@yahoo.com>
2025-05-08 03:03:23 -0600

    Update IntegrationsMain.tsx

--- app/_components/Integrations/IntegrationsMain.tsx
+++ app/_components/Integrations/IntegrationsMain.tsx
@@ -1,12 +1,12 @@
 'use client';
 
 import React from 'react'
-import { integrations, intergrations2 } from './Skills';
+import { integrations, intergrations2, myTechStack } from './Skills';
 import Integrations from './Integrations';
 import { useTranslations } from 'next-intl';
-import Tech from './Tech';
 import { motion } from "framer-motion";
 import { fadeIn } from "@/motion";
+import TechStack from './TechStack';
 
 const AboutMain = () => {
   const t = useTranslations('Integrations')
@@ -18,7 +18,7 @@ const AboutMain = () => {
       whileInView="show"
       viewport={{ once: true }}
       id='skills'
-      className=' overflow-hidden px-4 lg:px-0 py-16 md:py-40 mx-auto'>
+      className=' overflow-hidden px-4 lg:px-0 pt-40 pb-6 flex items-center min-h-screen mx-auto'>
       <div className='px-4 max-w-[1200px] mx-auto'>
         <div
           className='grid lg:grid-cols-2 items-center lg:gap-16'>
@@ -29,17 +29,16 @@ const AboutMain = () => {
               </div>
               <p className='text-sm font-medium text-light-text dark:text-dark-text'>{t('Skills')}</p>
             </div>
-            <h2 className='text-7xl md:text-8xl text-wrap font-bold mt-6 text-light-text dark:text-dark-text'>{t('Push')} <span className='text-blue-primary dark:text-light-accent'>{t('Horizons')}</span></h2>
+            <h2 className='text-7xl md:text-8xl font-bold mt-6 text-light-text dark:text-dark-text'>{t('Push')} <span className='text-blue-primary dark:text-light-accent'>{t('Horizons')}</span></h2>
             <p className='text-light-text dark:text-dark-text mt-4 text-lg'>{t('Hobby_Coder')}</p>
           </div>
           <div>
-            <div className='h-[400px] lg:h-[500px] mt-8 lg:mt-0 overflow-hidden grid md:grid-cols-2 gap-4 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]'>
-              <Integrations integrations={[...integrations, ...intergrations2]} className='flex md:hidden' />
-              <Integrations integrations={integrations} className='hidden md:flex' />
-              <Integrations integrations={intergrations2} className='hidden md:flex' />
+            <div className='h-[400px] lg:h-[500px] mt-8 lg:mt-0 overflow-hidden gap-4 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]'>
+              <Integrations reverse integrations={myTechStack} className='hidden md:flex' />
             </div>
           </div>
         </div>
+        <TechStack />
       </div>
     </motion.section>
   )
